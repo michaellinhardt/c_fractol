1d0c44b0a33053f8fc039a833ae2e02216762e03
mlinhard <nestoyeur@gmail.com>
2016-12-15 19:44:08 +0100

    ok

--- srcs/ft_mlx_scene_img_push.c
+++ srcs/ft_mlx_scene_img_push.c
@@ -6,7 +6,7 @@
 /*   By: mlinhard <mlinhard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/15 04:11:31 by mlinhard          #+#    #+#             */
-/*   Updated: 2016/12/15 06:18:48 by mlinhard         ###   ########.fr       */
+/*   Updated: 2016/12/15 19:43:59 by mlinhard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,3 +22,4 @@ void		scene_img_push(t_data *d, t_dmlx *m, int i, t_img *img)
 	(void)m;
 	(void)img;
 }
+7
