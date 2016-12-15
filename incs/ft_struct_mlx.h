a3fa0ef6c17c9dc62d24f867dc4563bc956e088e
mlinhard <nestoyeur@gmail.com>
2016-12-15 04:04:05 +0100

    add objet struct

--- incs/ft_struct_mlx.h
+++ incs/ft_struct_mlx.h
@@ -6,7 +6,7 @@
 /*   By: mlinhard <mlinhard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/14 10:33:03 by mlinhard          #+#    #+#             */
-/*   Updated: 2016/12/14 10:33:15 by mlinhard         ###   ########.fr       */
+/*   Updated: 2016/12/15 04:03:56 by mlinhard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,26 @@ enum	e_scene {
 	FRACTOL_INIT,
 	FRACTOL
 };
+
+
+/*
+ * t_objet contiens les coordoné d'une image xpm (ou autre ?)
+ * la zone recouverte par l'image top[2], bot[2]
+ * ainsi que ça position pos[2] et son fade
+ * cut_mode indique comment découper l'objet dans le cas d'un menu
+ */
+typedef struct		s_objet
+{
+	t_img			xpm;
+	char			name[255];
+	int				top[2];
+	int				bot[2];
+	int				pos[2];
+	int				fade;
+	int				cut_mode;
+	int				width;
+	int				heigh;
+}					t_objet;
 
 typedef struct		s_img
 {
