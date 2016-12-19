7ded2b078a3d71117b6656d3e2600de050ba27e4
Michael Linhardt <nestoyeur@gmail.com>
2016-12-19 11:11:30 +0100

    mandelbrot and julia

--- srcs/ft_scene_1_fractol_draw.c
+++ srcs/ft_scene_1_fractol_draw.c
@@ -6,7 +6,7 @@
 /*   By: mlinhard <mlinhard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/19 00:40:00 by mlinhard          #+#    #+#             */
-/*   Updated: 2016/12/19 11:10:14 by mlinhard         ###   ########.fr       */
+/*   Updated: 2016/12/19 11:11:26 by mlinhard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,24 +34,16 @@ void		scene_1_draw_julia(t_img *lay, t_fract *param, t_fract *f)
 {
 	f->c.r = 0.285;
 	f->c.i = 0.01;
-	f->top.r = -1;
-	while ( ++f->top.r < param->delta.r )
+	f->z.r = (f->pos.r / WIN_X) * param->delta.r + param->top.r;
+	f->z.i = (f->pos.i / WIN_Y) * param->delta.i + param->top.i;
+	f->ite = 0;
+	while( f->z.r * f->z.r + f->z.i * f->z.i < 4 && f->ite < param->itemax )
 	{
-		f->top.i = -1;
-		while ( ++f->top.i < param->delta.i )
-		{
-			f->z.r = (f->pos.r / WIN_X) * param->delta.r + param->top.r;
-			f->z.i = (f->pos.i / WIN_Y) * param->delta.i + param->top.i;
-			f->ite = 0;
-			while( f->z.r * f->z.r + f->z.i * f->z.i < 4 && f->ite < param->itemax )
-			{
-				f->z = comp_add(comp_sqr(f->z), f->c);
-				f->ite++;
-			}
-			if ( f->ite == param->itemax )
-				lay->ptr[f->posi] = 0x0091fb30;
-			else
-				lay->ptr[f->posi] = 0x00b33e66;
-		}
+		f->z = comp_add(comp_sqr(f->z), f->c);
+		f->ite++;
 	}
+	if ( f->ite == param->itemax )
+		lay->ptr[f->posi] = 0x0091fb30;
+	else
+		lay->ptr[f->posi] = 0x00b33e66;
 }
