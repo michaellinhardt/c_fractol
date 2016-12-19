47b4ad36aba539715b593d93bf6b9603bb7dc7c7
Michael Linhardt <nestoyeur@gmail.com>
2016-12-19 11:10:15 +0100

    mandelbrot and julia

--- srcs/ft_scene_1_fractol_draw.c
+++ srcs/ft_scene_1_fractol_draw.c
@@ -6,7 +6,7 @@
 /*   By: mlinhard <mlinhard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/19 00:40:00 by mlinhard          #+#    #+#             */
-/*   Updated: 2016/12/19 11:09:13 by mlinhard         ###   ########.fr       */
+/*   Updated: 2016/12/19 11:10:14 by mlinhard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,14 +32,14 @@ void		scene_1_draw_mandelbrot(t_img *lay, t_fract *param, t_fract *f)
 
 void		scene_1_draw_julia(t_img *lay, t_fract *param, t_fract *f)
 {
+	f->c.r = 0.285;
+	f->c.i = 0.01;
 	f->top.r = -1;
 	while ( ++f->top.r < param->delta.r )
 	{
 		f->top.i = -1;
 		while ( ++f->top.i < param->delta.i )
 		{
-			f->c.r = 0.285;
-			f->c.i = 0.01;
 			f->z.r = (f->pos.r / WIN_X) * param->delta.r + param->top.r;
 			f->z.i = (f->pos.i / WIN_Y) * param->delta.i + param->top.i;
 			f->ite = 0;
